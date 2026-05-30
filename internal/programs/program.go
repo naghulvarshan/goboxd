6338aabda0fdc44c34bf518bc2532a0c53aa4680
naghulvarshan <naghulvarshanbs@gmail.com>
2026-05-30 20:51:28 +0530

    cleanup: splitting functions in program.go

--- internal/programs/program.go
+++ internal/programs/program.go
@@ -21,11 +21,73 @@ import (
 func Run(input *types.ProgramInfo, defaultArgs string, languageConfig types.LanguageSettings) (*types.Response, error) {
 
 	// Step 1: Initialize a directory to execute the code
+	baseDir, id, err := generateWorkSpace()
+	if err != nil {
+		return nil, err
+	}
 
+	// Step 2: Write the program to a file
+	filename := languageConfig.FileName
+	if input.SourceFileName != nil {
+		filename = *input.SourceFileName
+	}
+	if err = addSource(filename, baseDir, input.Source); err != nil {
+		return nil, err
+	}
+
+	// Step 3: Create test directories
+	if err = createTestWS(baseDir, input.Tests); err != nil {
+		return nil, err
+	}
+
+	// Step 4: Compile if needed
+	output := &types.Response{
+		Status:      "success",
+		TestOutputs: []types.TestOutput{},
+	}
+	if languageConfig.CompilationOpts != nil {
+		binary := languageConfig.BinaryFileName
+		if binary == nil {
+			binary = input.ArtifaceFileName
+		}
+		if binary == nil {
+			return nil, errors.New("binary artifact name is required")
+		}
+		compileCode(baseDir, *binary, filename, input.Build,
+			languageConfig.CompilationOpts, output, defaultArgs)
+	}
+
+	// Step 5: Running code
+	binaryFilename := filename
+	if languageConfig.BinaryFileName != nil && *languageConfig.BinaryFileName != "TAKE_FROM_REQUEST" {
+		binaryFilename = *languageConfig.BinaryFileName
+	} else if input.ArtifaceFileName != nil {
+		binaryFilename = *input.ArtifaceFileName
+	}
+	output.TestOutputs = runCode(baseDir, id, defaultArgs, binaryFilename, input.Run, languageConfig.RuntimeOpts,
+		input.Tests)
+	os.RemoveAll(baseDir)
+	return output, nil
+}
+
+func idGenerator() (string, error) {
+	const charset = "abcdefghijklmnopqrstuvwxyz0123456789"
+	result := make([]byte, 5)
+	for i := range result {
+		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return "", err
+		}
+		result[i] = charset[n.Int64()]
+	}
+	return string(result), nil
+}
+
+func generateWorkSpace() (string, string, error) {
 	id, err := idGenerator()
 	if err != nil {
 		slog.Debug("error generating id", "error", err)
-		return nil, errors.New("internal server error")
+		return "", "", errors.New("internal server error")
 	}
 	home, _ := os.UserHomeDir()
 	baseDir := filepath.Join(home, "nsip_"+id)
@@ -36,145 +98,138 @@ func Run(input *types.ProgramInfo, defaultArgs string, languageConfig types.Lang
 			id, err = idGenerator()
 			if err != nil {
 				slog.Debug("error generating id", "error", err)
-				return nil, errors.New("internal server error")
+				return "", "", errors.New("internal server error")
 			}
 			baseDir = filepath.Join(home, "nsip_"+id)
 			err = os.Mkdir(baseDir, 0755)
 			if err != nil {
-				return nil, errors.New("internal server error")
+				return "", "", errors.New("internal server error")
 			}
 
 		}
 	}
 	os.Mkdir(baseDir+"/proc", 0755)
+	return baseDir, id, nil
+}
 
-	// Step 2: Write the program to a file
-	filename := languageConfig.FileName
-	if input.SourceFileName != nil {
-		filename = *input.SourceFileName
-	}
-	err = os.WriteFile(fmt.Sprintf("%s/%s", baseDir, filename), []byte(input.Source), 0755)
+func addSource(sourceName, baseDir, source string) error {
+	err := os.WriteFile(fmt.Sprintf("%s/%s", baseDir, sourceName), []byte(source), 0755)
 	if err != nil {
 		slog.Debug("error creating and writing to file", "error", err)
-		return nil, errors.New("internal server error")
+		return errors.New("internal server error")
 	}
+	return nil
+}
 
-	// Step 3: Create test directories
-	for i := range input.Tests {
+func createTestWS(baseDir string, tests []types.Tests) error {
+	var err error
+	for i := range tests {
 		testDir := fmt.Sprintf("test_%d", i)
 		err = os.Mkdir(fmt.Sprintf("%s/%s", baseDir, testDir), 0755)
 		if err != nil {
 			slog.Debug("error creating directory", "error", err)
-			return nil, errors.New("internal server error")
+			return errors.New("internal server error")
 		}
 
-		err = os.WriteFile(fmt.Sprintf("%s/%s/%s", baseDir, testDir, "input"), []byte(input.Tests[i].Stdin), 0755)
+		err = os.WriteFile(fmt.Sprintf("%s/%s/%s", baseDir, testDir, "input"), []byte(tests[i].Stdin), 0755)
 		if err != nil {
 			slog.Debug("error writing test case input", "error", err)
-			return nil, errors.New("internal server error")
+			return errors.New("internal server error")
 		}
 	}
+	return nil
+}
 
-	// Step 4: Compile if needed
-	output := types.Response{
-		Status: "success",
+func compileCode(baseDir, binaryName, filename string,
+	ipBuildOpts *types.LimitsAndFlags, languageCompilationOpts *types.Options,
+	output *types.Response, defaultArgs string) error {
+	slog.Debug("compiling program")
+	args := []string{"--rw", "--log", baseDir + "/log",
+		"-e", "--cwd", "/", "-c", baseDir}
+	// TODO: override from input if available
+	if ipBuildOpts.Limits.MaxProcesses != 0 {
+		args = append(args, "--rlimit_nproc", strconv.FormatInt(int64(ipBuildOpts.Limits.MaxProcesses), 10))
+	} else if languageCompilationOpts.ResourceLimits.MaxProcesses != 0 {
+		args = append(args, "--rlimit_nproc", strconv.FormatInt(int64(languageCompilationOpts.ResourceLimits.MaxProcesses), 10))
 	}
-	if languageConfig.CompilationOpts != nil {
-		slog.Debug("compiling program")
-		args := []string{"--rw", "--log", baseDir + "/log",
-			"-e", "--cwd", "/", "-c", baseDir}
-		// TODO: override from input if available
-		if input.Build.Limits.MaxProcesses != 0 {
-			args = append(args, "--rlimit_nproc", strconv.FormatInt(int64(input.Build.Limits.MaxProcesses), 10))
-		} else if languageConfig.CompilationOpts.ResourceLimits.MaxProcesses != 0 {
-			args = append(args, "--rlimit_nproc", strconv.FormatInt(int64(languageConfig.CompilationOpts.ResourceLimits.MaxProcesses), 10))
-		}
-		if input.Build.Limits.MemoryKB != 0 {
-			args = append(args, "--rlimit_as", strconv.FormatInt(input.Build.Limits.MemoryKB, 10))
-		} else if languageConfig.CompilationOpts.ResourceLimits.MemoryKB != 0 {
-			args = append(args, "--rlimit_as", strconv.FormatInt(languageConfig.CompilationOpts.ResourceLimits.MemoryKB, 10))
-		}
-		if input.Build.Limits.WallTime != 0 {
-			args = append(args, "--time_limit", strconv.FormatInt(int64(input.Build.Limits.WallTime), 10))
-		} else if languageConfig.CompilationOpts.ResourceLimits.WallTime != 0 {
-			args = append(args, "--time_limit", strconv.FormatInt(int64(languageConfig.CompilationOpts.ResourceLimits.WallTime), 10))
-		}
-		args = append(args, strings.Fields(defaultArgs)...)
-		fmt.Printf("defaultArgs: %q\n", defaultArgs)
-		fmt.Printf("after split: %q\n", strings.Fields(defaultArgs))
-		args = append(args, "--symlink", "/lib:/lib64") // To be removed
-		binary := languageConfig.BinaryFileName
-		if binary == nil {
-			binary = input.ArtifaceFileName
-		}
-		if binary == nil {
-			return nil, errors.New("binary artifact name is required")
-		}
-		compilation := languageConfig.CompilationOpts.Args
-		compilation = strings.ReplaceAll(compilation, "{{ EXTRA_ARGS }}",
-			strings.Join(input.Build.Flags, " "))
-		compilation = strings.ReplaceAll(compilation, "{{ FILENAME }}", filename)
-		args = append(args, "--", languageConfig.CompilationOpts.Path)
-		args = append(args, strings.Fields(compilation)...)
-		fmt.Printf("full args: %q\n", args)
-		cmd := exec.Command("/usr/local/bin/nsjail", args...)
-		start := time.Now()
-		out, err := cmd.CombinedOutput()
-		log.Println(string(out))
-		elapsed := time.Since(start).Milliseconds()
-		output.Build = &types.ExecutionDetails{
-			Status:   "success",
-			Duration: int(elapsed),
-		}
-		if err != nil {
-			logs, _ := os.ReadFile(baseDir + "/log")
-			output.Status = "compilation_error"
-			output.Build.Status = "error"
-			output.Build.STDErr = string(logs)
-			log.Println(err)
-			return &output, nil
-		}
-		output.Build.STDOut = string(out)
-		// filename = *binary
+	if ipBuildOpts.Limits.MemoryKB != 0 {
+		args = append(args, "--rlimit_as", strconv.FormatInt(ipBuildOpts.Limits.MemoryKB, 10))
+	} else if languageCompilationOpts.ResourceLimits.MemoryKB != 0 {
+		args = append(args, "--rlimit_as", strconv.FormatInt(languageCompilationOpts.ResourceLimits.MemoryKB, 10))
 	}
-
-	// Step 5: Running code
-	args := []string{"--rw", "-e", "--cwd", "/", "-c", baseDir}
-	if input.Run != nil && input.Run.Limits.MaxProcesses != 0 {
-		args = append(args, "--rlimit_nproc", strconv.FormatInt(int64(input.Run.Limits.MaxProcesses), 10))
-	} else if languageConfig.RuntimeOpts.ResourceLimits.MaxProcesses != 0 {
-		args = append(args, "--rlimit_nproc", strconv.FormatInt(int64(languageConfig.CompilationOpts.ResourceLimits.MaxProcesses), 10))
-	}
-	if input.Run != nil && input.Run.Limits.MemoryKB != 0 {
-		args = append(args, "--rlimit_as", strconv.FormatInt(input.Run.Limits.MemoryKB, 10))
-	} else if languageConfig.CompilationOpts.ResourceLimits.MemoryKB != 0 {
-		args = append(args, "--rlimit_as", strconv.FormatInt(languageConfig.CompilationOpts.ResourceLimits.MemoryKB, 10))
-	}
-	if input.Run != nil && input.Run.Limits.WallTime != 0 {
-		args = append(args, "--time_limit", strconv.FormatInt(int64(input.Run.Limits.WallTime), 10))
-	} else if languageConfig.CompilationOpts.ResourceLimits.WallTime != 0 {
-		args = append(args, "--time_limit", strconv.FormatInt(int64(languageConfig.CompilationOpts.ResourceLimits.WallTime), 10))
+	if ipBuildOpts.Limits.WallTime != 0 {
+		args = append(args, "--time_limit", strconv.FormatInt(int64(ipBuildOpts.Limits.WallTime), 10))
+	} else if languageCompilationOpts.ResourceLimits.WallTime != 0 {
+		args = append(args, "--time_limit", strconv.FormatInt(int64(languageCompilationOpts.ResourceLimits.WallTime), 10))
 	}
 	args = append(args, strings.Fields(defaultArgs)...)
-	path := languageConfig.RuntimeOpts.Path
-	rtArgs := languageConfig.RuntimeOpts.Args
-	rtArgs = strings.ReplaceAll(rtArgs, "{{ FILENAME }}", filename)
-	binaryFilename := filename
-	if languageConfig.BinaryFileName != nil && *languageConfig.BinaryFileName != "TAKE_FROM_REQUEST" {
-		binaryFilename = *languageConfig.BinaryFileName
-	} else if input.ArtifaceFileName != nil {
-		binaryFilename = *input.ArtifaceFileName
+	fmt.Printf("defaultArgs: %q\n", defaultArgs)
+	fmt.Printf("after split: %q\n", strings.Fields(defaultArgs))
+	args = append(args, "--symlink", "/lib:/lib64") // To be removed
+
+	compilation := languageCompilationOpts.Args
+	compilation = strings.ReplaceAll(compilation, "{{ EXTRA_ARGS }}",
+		strings.Join(ipBuildOpts.Flags, " "))
+	compilation = strings.ReplaceAll(compilation, "{{ FILENAME }}", filename)
+	args = append(args, "--", languageCompilationOpts.Path)
+	args = append(args, strings.Fields(compilation)...)
+	fmt.Printf("full args: %q\n", args)
+	cmd := exec.Command("/usr/local/bin/nsjail", args...)
+	start := time.Now()
+	out, err := cmd.CombinedOutput()
+	log.Println(string(out))
+	elapsed := time.Since(start).Milliseconds()
+	output.Build = &types.ExecutionDetails{
+		Status:   "success",
+		Duration: int(elapsed),
 	}
-	rtArgs = strings.ReplaceAll(rtArgs, "{{ BINARY_FILENAME }}", binaryFilename)
+	if err != nil {
+		logs, _ := os.ReadFile(baseDir + "/log")
+		output.Status = "compilation_error"
+		output.Build.Status = "error"
+		output.Build.STDErr = string(logs)
+		log.Println(err)
+		return nil
+	}
+	output.Build.STDOut = string(out)
+	// filename = *binary
+
+	return nil
+}
+
+func runCode(baseDir, id, defaultArgs, filename string, inputPref *types.LimitsAndFlags,
+	langOpts types.Options, tests []types.Tests) []types.TestOutput {
+	testResults := []types.TestOutput{}
+	args := []string{"--rw", "-e", "--cwd", "/", "-c", baseDir}
+	if inputPref != nil && inputPref.Limits.MaxProcesses != 0 {
+		args = append(args, "--rlimit_nproc", strconv.FormatInt(int64(inputPref.Limits.MaxProcesses), 10))
+	} else if langOpts.ResourceLimits.MaxProcesses != 0 {
+		args = append(args, "--rlimit_nproc", strconv.FormatInt(int64(langOpts.ResourceLimits.MaxProcesses), 10))
+	}
+	if inputPref != nil && inputPref.Limits.MemoryKB != 0 {
+		args = append(args, "--rlimit_as", strconv.FormatInt(inputPref.Limits.MemoryKB, 10))
+	} else if langOpts.ResourceLimits.MemoryKB != 0 {
+		args = append(args, "--rlimit_as", strconv.FormatInt(langOpts.ResourceLimits.MemoryKB, 10))
+	}
+	if inputPref != nil && inputPref.Limits.WallTime != 0 {
+		args = append(args, "--time_limit", strconv.FormatInt(int64(inputPref.Limits.WallTime), 10))
+	} else if langOpts.ResourceLimits.WallTime != 0 {
+		args = append(args, "--time_limit", strconv.FormatInt(int64(langOpts.ResourceLimits.WallTime), 10))
+	}
+	args = append(args, strings.Fields(defaultArgs)...)
+	path := langOpts.Path
+	rtArgs := langOpts.Args
+	rtArgs = strings.ReplaceAll(rtArgs, "{{ FILENAME }}", filename)
+	rtArgs = strings.ReplaceAll(rtArgs, "{{ BINARY_FILENAME }}", filename)
 	args = append(args, path)
 	args = append(args, strings.Fields(rtArgs)...)
-	for i := range input.Tests {
+	for i := range tests {
 		testDir := fmt.Sprintf("test_%d", i)
 		ipFile := fmt.Sprintf("%s/%s/%s", baseDir, testDir, "input")
 
 		ipFileCont, err := os.ReadFile(ipFile)
 		if err != nil {
-			output.TestOutputs = append(output.TestOutputs, types.TestOutput{
+			testResults = append(testResults, types.TestOutput{
 				ExecutionDetails: types.ExecutionDetails{
 					Status:   "error",
 					Duration: 0,
@@ -228,25 +283,10 @@ func Run(input *types.ProgramInfo, defaultArgs string, languageConfig types.Lang
 			)
 			testRes.Status = "errored"
 			testRes.STDErr = stderr.String()
-		} else if string(out) != input.Tests[i].ExpectedOut {
+		} else if string(out) != tests[i].ExpectedOut {
 			testRes.ExecutionDetails.Status = "wrong_output"
 		}
-		output.TestOutputs = append(output.TestOutputs, testRes)
-		log.Println(string(out))
+		testResults = append(testResults, testRes)
 	}
-	os.RemoveAll(baseDir)
-	return &output, nil
-}
-
-func idGenerator() (string, error) {
-	const charset = "abcdefghijklmnopqrstuvwxyz0123456789"
-	result := make([]byte, 5)
-	for i := range result {
-		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
-		if err != nil {
-			return "", err
-		}
-		result[i] = charset[n.Int64()]
-	}
-	return string(result), nil
+	return testResults
 }
